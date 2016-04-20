4d526cdfcfe37ce8fb1df4f7973072a4473b5aa5
NickolasDePiton <crysis4997@ya.ru>
2016-04-20 21:34:39 +0300

    Update matrix.h

--- include/matrix.h
+++ include/matrix.h
@@ -7,50 +7,50 @@
 using namespace std;
 
 template <class T>
-	class Matrix;
-
-	template <class T>
-	ostream & operator<<(ostream & output, const Matrix<T> &);
-
-	template <class T>
-	istream & operator>>(istream & input, Matrix<T> &);
+class Matrix;
 
 class Isclucheniya {
-public:
-	Isclucheniya(char* _err);
-	char* what();
-private:
 	char* err;
+public:
+	Isclucheniya(char* _err) : err(_err) {}
+	virtual char* what() { return err; }
 };
 
 class Razmery : public Isclucheniya{
 	char* err;
 public:
-	Razmery();
-	char* what();
+	Razmery() : Isclucheniya("ERROR: nevernye razmery!") {};
+	char* what() { return err; };
 };
 
 class WrongIndex : public Isclucheniya{
 	char* err;
 public:
-	WrongIndex();
-	char* what();
+	WrongIndex() : Isclucheniya("ERROR: wrong index!") {};
+	char* what() { return err; };
 };
 
 class FileNotOpen : public Isclucheniya{
 	char* err;
 public:
-	FileNotOpen();
-	char* what();
+	FileNotOpen() : Isclucheniya("ERROR: file not open!") {};
+	char* what() { return err; };
 };
 
 class Samoprisvaivanie: public Isclucheniya{
 	char* err;
 public:
-	Samoprisvaivanie();
-	char* what();
+	Samoprisvaivanie() : Isclucheniya("ERROR: samoprisvaivanie!") {};
+	char* what() { return err; };
 };
 
+template <class T>
+ostream & operator<<(ostream & output, const Matrix<T> &);
+
+template <class T>
+istream & operator>>(istream & input, Matrix<T> &);
+
+template <class T>
 
 template <class T>
 class Matrix{
@@ -65,8 +65,8 @@ public:
 	Matrix operator +(const Matrix &array) const;
 	Matrix operator *(const Matrix &array) const;
 	T* operator [] (int k) const;
-	int cout_lines() const;
-	int cout_columns() const;
+	int get_lines() const;
+	int get_columns() const;
 	Matrix& operator = (const Matrix &a);
 	bool operator ==(const Matrix &array) const;
 	friend ostream & operator<< <>(ostream & output, const Matrix<T> & matrix);
